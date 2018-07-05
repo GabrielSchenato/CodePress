804e329bd3d33417dfc2242e3b7a32297b177c0a
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-05 10:03:59 -0300

    Acceptance tests tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,6 +71,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                         <a href="{{ url('/admin/categories') }}">Categories</a>
+                        <a href="{{ url('/admin/tags') }}">Tags</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
---
+++ tests/CodeCategory/Acceptance/AdminTagsTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace CodePress\CodeTag\Acceptance\Tests;
+
+use CodePress\CodeTag\Models\Tag;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+/**
+ * Description of AdminTagsTest
+ *
+ * @author gabriel
+ */
+class AdminTagsTest extends DuskTestCase
+{
+    
+
+
+    public function test_can_visit_admin_tags_page()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin/tags')
+                    ->assertSee('Tags');
+        });
+    }
+    
+    public function test_tags_listing()
+    {
+        Tag::create(['name' => 'Tag 1']);
+        Tag::create(['name' => 'Tag 2']);
+        Tag::create(['name' => 'Tag 3']);
+        Tag::create(['name' => 'Tag 4']);
+        
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin/tags')
+                    ->assertSee('Tag 1')
+                    ->assertSee('Tag 2')
+                    ->assertSee('Tag 3')
+                    ->assertSee('Tag 4');
+        });
+        Tag::truncate();
+    }
+    
+    public function test_click_create_new_tag()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin/tags')
+                    ->clickLink("Create Tag")
+                    ->assertPathIs('/admin/tags/create');
+        });
+    }
+    
+    public function test_create_new_tag()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin/tags/create')
+                    ->type('name', 'Tag Test')
+                    ->press('Create Tag')
+                    ->assertPathIs('/admin/tags')
+                    ->assertSee('Tag Test');
+        });
+    }
+    
+    public function test_click_edit_tag()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin/tags')
+                    ->clickLink("Edit tag")
+                    ->assertPathIs('/admin/tags/1/edit');
+        });
+    }
+    
+    public function test_edit_tag()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin/tags/1/edit')
+                    ->type('name', 'Tag Edited')
+                    ->press('Edit Tag')
+                    ->assertPathIs('/admin/tags')
+                    ->assertSee('Tag Edited');
+        });
+    }
+    
+    public function test_delete_tag()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin/tags')
+                    ->press("Delete tag")
+                    ->assertDontSee('Tag Edited');
+        });
+    }
+
+}
