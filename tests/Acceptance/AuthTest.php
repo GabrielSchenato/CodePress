dc5a9fa0325c66d90d24509edc7850e04a66b55a
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-13 15:24:27 -0300

    Testando login e autenticação

---
+++ tests/Acceptance/AuthTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+/**
+ * Description of AuthTest
+ *
+ * @author gabriel
+ */
+class AuthTest extends DuskTestCase
+{
+
+    public function test_can_login_in_application()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('email', 'gabriel.schenato@inovadora.com.br')
+                    ->type('password', '123456')
+                    ->press('Login')
+                    ->assertPathIs('/home')
+                    ->assertSee('Dashboard');
+            $browser->driver->manage()->deleteAllCookies();
+        });
+    }
+    
+    public function test_can_not_login_in_application()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('email', 'teste@inovadora.com.br')
+                    ->type('password', 'asdsa')
+                    ->press('Login')
+                    ->assertPathIs('/login')
+                    ->assertSee('Password');
+        });
+    }
+
+}
