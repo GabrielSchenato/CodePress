b14f441ad982f6058684dc1f6c491b0decb926e6
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-16 11:54:48 -0300

    Criando teste para template email.registration

---
+++ tests/Acceptance/Template/EmailRegistrationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+/**
+ * Description of EmailRegistrationTest
+ *
+ * @author gabriel
+ */
+class EmailRegistrationTest extends DuskTestCase
+{
+
+    protected function createRoute()
+    {
+        Route::middleware(['web'])->group(function () {
+            Route::get('/email/registration', function () {
+                return view('email.registration', [
+                    'username' => 'teste@teste.com',
+                    'password' => '123456'
+                ]);
+            });
+        });
+    }
+
+    public function test_can_generate_html_from_template()
+    {
+        $this->createRoute();
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/email/registration')
+                    ->assertSee('teste@teste.com')
+                    ->assertSee('123456');
+        });
+    }
+
+}
